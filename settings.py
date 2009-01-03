0a2cfcffed3a40a30db60172ea4384c332770f42
bwilson <doubtingben@gmail.com>
2009-01-03 17:03:57 -0500

    Removed password....

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 #DATABASE_ENGINE = 'mysql'           # 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
 #DATABASE_NAME = 'mysite'             # Or path to database file if using sqlite3.
 #DATABASE_USER = 'mysite'            # Not used with sqlite3.
-#DATABASE_PASSWORD = '23@#weWE'         # Not used with sqlite3.
+#DATABASE_PASSWORD = ''         # Not used with sqlite3.
 #DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
 #DATABASE_PORT = ''             # Set to empty string for default. Not used with sqlite3.
 
